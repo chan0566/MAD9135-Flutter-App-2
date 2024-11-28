53018aa73ddd49ceefcd88ffb0078567ad9654a3
Dylan Chan <chan0566@algonquinlive.com>
2024-11-28 15:35:26 -0500

    Add ContactPage

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   int _currentIndex = 0;
 
-  List<Widget> _screen = [HomePage(), DataPage()];
+  List<Widget> _screen = [HomePage(), DataPage(), ContactPage()];
 
   @override
   Widget build(BuildContext context) {
@@ -48,6 +48,8 @@ class _MainScreenState extends State<MainScreen> {
         destinations: [
           NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
           NavigationDestination(icon: Icon(Icons.data_usage), label: 'Data'),
+          NavigationDestination(
+              icon: Icon(Icons.contact_mail), label: 'Contact')
         ],
       ),
     );
@@ -148,7 +150,7 @@ class _DataPageState extends State<DataPage> {
 
   Future<List<Product>> fetchData() async {
     final response =
-        await http.get(Uri.parse('https://dummyjson.com/products'));
+        await http.get(Uri.parse('https://dummyjson.com/products?limit=20'));
     print(response.body);
     if (response.statusCode == 200) {
       final List<dynamic> jsonData = json.decode(response.body)['products'];
@@ -176,4 +178,107 @@ class Product {
       required this.title,
       required this.description,
       required this.thumbnail});
+}
+
+class ContactPage extends StatefulWidget {
+  const ContactPage({super.key});
+
+  @override
+  State<ContactPage> createState() => _ContactPageState();
+}
+
+class _ContactPageState extends State<ContactPage> {
+  final _formKey = GlobalKey<FormState>();
+  String? _name;
+  String? _email;
+  String? _message;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  Text("Contact Us",
+                      style: Theme.of(context).textTheme.bodyLarge),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    autofocus: true,
+                    decoration: InputDecoration(
+                      labelText: 'Name',
+                      hintText: 'Enter your name',
+                      icon: Icon(Icons.person),
+                    ),
+                    keyboardType: TextInputType.name,
+                    textInputAction: TextInputAction.next,
+                    style: const TextStyle(color: Colors.black),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your name';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _name = value;
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      hintText: 'Enter your email',
+                      icon: Icon(Icons.email),
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                    textInputAction: TextInputAction.next,
+                    style: const TextStyle(color: Colors.black),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your email';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _email = value;
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Message',
+                      hintText: 'Enter your message',
+                      icon: Icon(Icons.message),
+                    ),
+                    keyboardType: TextInputType.multiline,
+                    maxLines: 3,
+                    style: const TextStyle(color: Colors.black),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your message';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _message = value;
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _formKey.currentState!.save();
+                      }
+                    },
+                    child: Text('Submit'),
+                  )
+                ],
+              )),
+        ),
+      ),
+    );
+  }
 }
